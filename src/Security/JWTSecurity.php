7c21027922bc9828b8455e1e76fe0368c86a8857
Romain Bouchez <adrian.bouchez@laposte.net>
2022-04-07 10:16:03 +0200

    Token + exp + skiptoken

--- src/Security/JWTSecurity.php
+++ src/Security/JWTSecurity.php
@@ -2,31 +2,52 @@
 namespace App\Security;
 
 use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 class JWTSecurity {
 
   private $secret = "Ceci est un secret";
   private $algo = "HS256";
+  private $skipToken = true;
 
-  private array $payload;
-
-  public function generateToken (): string
-  {
-    // $date = new \DateTime();
-    // $exp = $date->modify("+ 1 day");
-
-    $this->payload = [
+  private array $payload = [
       "iss" => "nous",
+      "exp" => 0,
       "bidule" => "Bienvenu dans nous."
     ];
 
+  public function generateToken (): string
+  {
+    $this->payload['exp'] = time() + (24*60*60);
     return JWT::encode($this->payload, $this->secret, $this->algo);
   }
 
   public function verifyToken () {
-    if (isset($_COOKIE['JWT']) && !empty($_COOKIE['JWT'])) {
-      
+    if ($this->skipToken) {
+      return true;
     }
 
+    if ( !isset($_COOKIE['JWT']) || empty($_COOKIE['JWT'])) {
+      return false;
+    }
+
+    $token = $_COOKIE['JWT'];
+    $decode = JWT::decode($token, new Key($this->secret, $this->algo));
+
+    if (!(
+      isset($decode->iss) && isset($decode->exp) && isset($decode->bidule)
+    )) {
+      return false;
+    }
+
+    if (
+      $decode->iss == $this->payload['iss'] &&
+      $decode->exp > time() &&
+      $decode->bidule == $this->payload['bidule']
+      ) {
+      return true;
+    }
+
+    return false;
   }
 }
